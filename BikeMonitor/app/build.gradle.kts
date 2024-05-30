782ab42a4d2bc79333c301df4abc26e19e93c5c4
bmt2211 <bmt221100@gmail.com>
2024-05-30 16:01:32 +0700

    Firebase setup

--- BikeMonitor/app/build.gradle.kts
+++ BikeMonitor/app/build.gradle.kts
@@ -49,6 +49,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database")
 
 }
