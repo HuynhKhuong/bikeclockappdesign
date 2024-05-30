e1a09a4b60fafd330db83a0b46343bbcdaebe7b3
bmt2211 <bmt221100@gmail.com>
2024-05-30 16:09:42 +0700

    Firebase setup resolve conflic

--- BikeMonitor/app/build.gradle.kts
+++ BikeMonitor/app/build.gradle.kts
@@ -45,10 +45,15 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.android.support:support-v4:23.2.1")
+    implementation("com.google.android.material:material:1.2.0")
+    implementation("com.github.leandroborgesferreira:loading-button-android:2.3.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 
 }
