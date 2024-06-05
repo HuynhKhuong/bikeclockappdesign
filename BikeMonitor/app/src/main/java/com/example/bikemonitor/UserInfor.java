e43a3feb90db2bfa9b46b99b1eea2c1678bf4a92
bmt2211 <bmt221100@gmail.com>
2024-06-05 19:13:43 +0700

    Update Register method, Firebase and user class

---
+++ BikeMonitor/app/src/main/java/com/example/bikemonitor/UserInfor.java
@@ -0,0 +1,127 @@
+package com.example.bikemonitor;
+
+public class UserInfor {
+    private String m_userName;
+    private String m_userEmail;
+    private String m_userMobileNo;
+    private String m_userPassword;
+    private String m_userID;
+
+    // Recorded class for each user
+    static class RecordedAttribute{
+        private int m_userDistance;
+        private int m_userAvrVelocity;
+        private int m_userDateRecorded;
+        private int m_userTimeRecorded;
+        private int m_userDevice;
+        private int m_order;
+
+        public RecordedAttribute(int order, int distance, int avr_velo, int date, int time, int device)
+        {
+            this.m_order = order;
+            this.m_userDistance = distance;
+            this.m_userAvrVelocity = avr_velo;
+            this.m_userDateRecorded = date;
+            this.m_userTimeRecorded = time;
+            this.m_userDevice = device;
+        }
+
+        public int getUserDistance()
+        {
+            return m_userDistance;
+        }
+
+        public int getUserAverageVelocity()
+        {
+            return m_userAvrVelocity;
+        }
+
+        public int getUserDateRecorded()
+        {
+            return m_userDateRecorded;
+        }
+
+        public int getUserTimeRecorded()
+        {
+            return m_userTimeRecorded;
+        }
+
+        public int getUserDevice()
+        {
+            return m_userDevice;
+        }
+
+        public int getOrder()
+        {
+            return m_order;
+        }
+
+    }
+
+
+    // UserInfor Constructor for Log-in action
+    public UserInfor (String user_email, String user_password)
+    {
+        m_userEmail = user_email;
+        m_userPassword = user_password;
+    }
+
+    // UserInfor Constructor for Register action
+    public UserInfor (String user_name, String user_email, String user_mobileNo, String user_password)
+    {
+        m_userName = user_name;
+        m_userEmail = user_email;
+        m_userMobileNo = user_mobileNo;
+        m_userPassword = user_password;
+    }
+    public void setUserName(String user_name)
+    {
+        this.m_userName = user_name;
+    }
+
+    public String getUserName()
+    {
+        return m_userName;
+    }
+
+    public void setUserEmail(String user_email)
+    {
+        this.m_userEmail = user_email;
+    }
+
+    public String getUserEmail()
+    {
+        return m_userEmail;
+    }
+
+    public void setUserMobileNo(String user_mobileno)
+    {
+        this.m_userMobileNo = user_mobileno;
+    }
+
+    public String getUserMobileNo()
+    {
+        return m_userMobileNo;
+    }
+
+    public void setUserPassword(String user_password)
+    {
+        this.m_userPassword = user_password;
+    }
+
+    public String getUserPassword()
+    {
+        return m_userPassword;
+    }
+
+    public void setUserID(String user_id)
+    {
+        this.m_userID = user_id;
+    }
+    public String getUserID()
+    {
+        return m_userID;
+    }
+
+
+}
--- BikeMonitor/app/src/main/java/com/example/bikemonitor/ui/register/RegisterFragment.java
+++ BikeMonitor/app/src/main/java/com/example/bikemonitor/ui/register/RegisterFragment.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,8 +21,111 @@ import com.example.bikemonitor.databinding.FragmentRegisterBinding;
 import com.github.leandroborgesferreira.loadingbutton.customViews.CircularProgressButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+// Firebase package
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.ChildEventListener;
+
 public class RegisterFragment extends Fragment {
     private FragmentRegisterBinding binding;
+
+    // Firebase database reference
+    private DatabaseReference m_Ref;
+    private String m_userName;
+    private String m_userEmail;
+    private String m_userMobileNo;
+    private String m_userPassword;
+    private String m_userID;
+
+    public static final int invalidEmail = 0;
+    public static final int invalidPhoneNo = 1;
+    public static final int EmailUsed = 2;
+    public static final int emptyUser = 3;
+    public static final int successfullyReg = 4;
+
+    private void alertAction(int alertType)
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        switch(alertType) {
+            case emptyUser:
+                alertDialogBuilder.setMessage("Please fill user Information");
+                alertDialogBuilder.setTitle("User Information missed");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case EmailUsed:
+                alertDialogBuilder.setMessage("Please sign up other Email");
+                alertDialogBuilder.setTitle("Email already used !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case invalidEmail:
+                alertDialogBuilder.setMessage("Please sign up correct Email format");
+                alertDialogBuilder.setTitle("Email format is incorrect !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case invalidPhoneNo:
+                alertDialogBuilder.setMessage("Please sign up correct phone Number");
+                alertDialogBuilder.setTitle("Phone number format is incorrect !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case successfullyReg:
+                alertDialogBuilder.setMessage("Congratulation");
+                alertDialogBuilder.setTitle("Your account registered successfully !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+
+        }
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+    private boolean checkEmailFormat(String userEmail){
+        String emailAT = userEmail.split("@")[1];
+        return emailAT.equals("gmail.com");
+    }
+
+    private boolean checkPhoneFormat(String userPhoneNo){
+        int phoneNoLength = userPhoneNo.length();
+        return phoneNoLength == 10; // Phone Number Length must be 10 in Vietnam area
+    }
+
+    private boolean checkMailAlreadyUsed(String userID, String currentUser)
+    {
+            ///Temporarily not used
+//        if (userID.equals(currentUser.split("@")[0])){
+//            m_Ref.child(userID).child("Email2checkcc").setValue("true");
+//            m_Ref.child(userID).child("userID2Check").setValue(userID);
+//            m_Ref.child(userID).child("current").setValue(currentUser.split("@")[0]);
+//            return true;
+//        }
+//        m_Ref.child(userID).child("Email2checkcc").setValue("false");
+//        m_Ref.child(userID).child("userID2Check").setValue(userID);
+//        m_Ref.child(userID).child("current").setValue(currentUser.split("@")[0]);
+//        return false;
+        return false;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,11 +155,62 @@ public class RegisterFragment extends Fragment {
             }
         });
 
+        // Firebase reference
+        m_Ref = FirebaseDatabase.getInstance().getReference();
+        TextView m_nameText = binding.editTextName;
+        TextView m_emailText = binding.editTextEmail;
+        TextView m_mobileNoText = binding.editTextMobile;
+        TextView m_passwordText = binding.editTextPassword;
+
         CircularProgressButton registerButton = binding.cirRegisterButton;
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do nothing
+                m_userName = m_nameText.getText().toString();
+                m_userEmail = m_emailText.getText().toString();
+                m_userMobileNo = m_mobileNoText.getText().toString();
+                m_userPassword = m_passwordText.getText().toString();
+
+                // Variable to check validity of user info
+                boolean l_emailIsOK;
+                boolean l_phoneNumberOK;
+                boolean l_emailIsUsed;
+                String l_currentUser;
+
+                // get character before AT in email for user ID
+                m_userID = m_userEmail.split("@")[0];
+
+                if(m_userID != null && !m_userID.trim().isEmpty() &&
+                        m_userMobileNo != null && !m_userMobileNo.trim().isEmpty() &&
+                        m_userPassword != null && !m_userPassword.trim().isEmpty()) {
+                    l_emailIsOK = checkEmailFormat(m_userEmail);
+                    l_phoneNumberOK = checkPhoneFormat(m_userMobileNo);
+
+//                    l_currentUser = (m_Ref.child(m_userID).child("Email").get()).toString();
+//                    l_emailIsUsed = checkMailAlreadyUsed(m_userID, l_currentUser);
+                    if(!l_emailIsOK){
+                        alertAction(invalidEmail);
+                    }
+                    else if (!l_phoneNumberOK){
+                        alertAction(invalidPhoneNo);
+                    }
+//                    else if (l_emailIsUsed)
+//                        alertAction(EmailUsed);
+//                    }
+
+                    if(l_emailIsOK && l_phoneNumberOK)
+                    {
+                        m_Ref.child(m_userID).child("Name").setValue(m_userName);
+                        m_Ref.child(m_userID).child("Email").setValue(m_userEmail);
+                        m_Ref.child(m_userID).child("Mobile").setValue(m_userMobileNo);
+                        m_Ref.child(m_userID).child("Password").setValue(m_userPassword);
+                        alertAction(successfullyReg);
+                    }
+
+                }
+                else{
+                    alertAction(emptyUser);
+                }
             }
         });
         return root;
