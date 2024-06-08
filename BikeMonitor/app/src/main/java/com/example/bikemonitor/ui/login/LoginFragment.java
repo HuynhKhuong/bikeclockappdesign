c67c74c4e2d233e2784cf44bb90f3e6d0444b5a2
bmt2211 <bmt221100@gmail.com>
2024-06-08 16:38:24 +0700

    Update Log-in method, userInfor data

--- BikeMonitor/app/src/main/java/com/example/bikemonitor/ui/login/LoginFragment.java
+++ BikeMonitor/app/src/main/java/com/example/bikemonitor/ui/login/LoginFragment.java
@@ -1,10 +1,14 @@
 package com.example.bikemonitor.ui.login;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +19,149 @@ import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+// Firebase package
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.ChildEventListener;
+
 import com.example.bikemonitor.R;
 import com.example.bikemonitor.databinding.FragmentLoginBinding;
 
+import com.example.bikemonitor.UserInfor;
+import java.util.Objects;
+
 public class LoginFragment extends Fragment {
     private FragmentLoginBinding binding;
+    // Firebase reference
+    private DatabaseReference m_Ref;
+
+    // userEmail, userPassword from firebase
+    public String f_userEmail, f_userPassword;
+    private int logIn_status;
+
+    // Enumerate status of email, password
+    final int e_niceInfo = 0;
+    final int e_emailMissed = 1;
+    final int e_passwordMissed = 2;
+    final int e_EmailWrongFormat = 3;
+    final int e_EmailNotRegistered = 4;
+    final int e_WrongPassword = 5;
+    final int e_userLogInSuccessfully = 6;
+
+    private UserInfor m_userInfo;
+
+    public interface FirebaseCallback {
+        void onLoginStatusChanged(boolean loginStatus, String email, String password);
+    }
+
+    private void VerifyDataWithFirebase(DatabaseReference dtbRef, String userID,
+                                        String userEmail, String userPassword,
+                                        UserInfor m_user, FirebaseCallback callback){
+        dtbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                f_userEmail = dataSnapshot.child(userID).child("Email").getValue(String.class);
+                f_userPassword = dataSnapshot.child(userID).child("Password").getValue(String.class);
+                if (!Objects.equals(userEmail, f_userEmail)){
+                    alertAction(e_EmailNotRegistered);
+                    m_user.setLogInSts(false);
+                }
+                else if (!Objects.equals(userPassword, f_userPassword)){
+                    alertAction(e_WrongPassword);
+                    m_user.setLogInSts(false);
+                }
+                else{
+                    m_user.setLogInSts(true);
+                }
+                callback.onLoginStatusChanged(m_user.getLogInSts(),userEmail,userPassword);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Do nothing - update later if necessary
+            }
+
+        });
+    }
+
+    private int ValidateUserInfo(String userEmail, String userPassword) {
+        int retStatus = e_niceInfo;
+        if(userEmail.isEmpty()){
+            retStatus = e_emailMissed;
+        }
+        else if(userEmail.contains("@")){
+            String l_afterID = userEmail.split("@")[1];
+            if(!l_afterID.equals("gmail.com")){
+                retStatus = e_EmailWrongFormat;
+            }
+        }
+        else if(!userEmail.contains("@")){
+            retStatus = e_EmailWrongFormat;
+        }
+        else if(userPassword.isEmpty()){
+            retStatus = e_passwordMissed;
+        }
+        return retStatus;
+
+    }
+
+    private void alertAction(int alertType)
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        switch(alertType) {
+            case e_emailMissed:
+                alertDialogBuilder.setMessage("Please fill-in your Email");
+                alertDialogBuilder.setTitle("Missing Email");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case e_passwordMissed:
+                alertDialogBuilder.setMessage("Please fill-in password");
+                alertDialogBuilder.setTitle("Missing password");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case e_EmailWrongFormat:
+                alertDialogBuilder.setMessage("Please sign in correct Email format");
+                alertDialogBuilder.setTitle("Email format is incorrect !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case e_EmailNotRegistered:
+                alertDialogBuilder.setMessage("Please sign-up to continue");
+                alertDialogBuilder.setTitle("Your account invalid !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+            case e_WrongPassword:
+                alertDialogBuilder.setMessage("Please fill correct password");
+                alertDialogBuilder.setTitle("Your password is incorrect !");
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setNegativeButton("Exit", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                break;
+
+        }
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,16 +170,53 @@ public class LoginFragment extends Fragment {
 
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Firebase reference
+        m_Ref = FirebaseDatabase.getInstance().getReference();
+        TextView m_emailText = binding.editTextEmail;
+        TextView m_passwordText = binding.editTextPassword;
+        m_userInfo = new UserInfor();
+
         binding.cirLoginButton.setOnClickListener(
         new View.OnClickListener() {
               @Override
               public void onClick(View v) {
                   binding.cirLoginButton.startAnimation();
-                  ///Some asynchronous task 
+                  ///Some asynchronous task
+
+                  // get info from Textview
+                  String l_email = m_emailText.getText().toString();
+                  String l_password = m_passwordText.getText().toString();
+                  String l_userID;
+                  int l_validateInfo;
+                  // Validate email and password is valid
+                  l_validateInfo = ValidateUserInfo(l_email,l_password);
+                  if(l_validateInfo != e_niceInfo){
+                      alertAction(l_validateInfo);
+                  }
+                  else {
+                      // Get user Email and password from firebase to verify
+                      l_userID = l_email.split("@")[0];
+                      VerifyDataWithFirebase(m_Ref, l_userID, l_email, l_password, m_userInfo, new FirebaseCallback() {
+                          @Override
+                          public void onLoginStatusChanged(boolean loginStatus, String email, String password) {
+                              if(loginStatus){
+                                  m_userInfo.setUserEmail(email);
+                                  m_userInfo.setUserPassword(password);
+                                  Navigation.findNavController(binding.getRoot()).navigate(R.id.action_nav_login_to_nav_gallery);
+                              }
+                              // Do something with the login status
+                              // For example, navigate to another screen
+                              // Navigation.findNavController(binding.getRoot()).navigate(R.id.action_nav_login_to_nav_gallery);
+                              m_userInfo.setLogInSts(loginStatus);
+                          }
+                      });
+                  }
+
                   ///move to other state
                   binding.cirLoginButton.stopAnimation();
 
-                  Navigation.findNavController(binding.getRoot()).navigate(R.id.action_nav_login_to_nav_gallery);
+                  //Navigation.findNavController(binding.getRoot()).navigate(R.id.action_nav_login_to_nav_gallery);
               }
           }
         );
